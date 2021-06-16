443985d840b59c32164819c359ca892377738de8
johnpmeyers20 <johnpmeyers20@yahoo.com>
2021-06-16 17:02:21 -0400

    initial commit. creation and styling of modal

---
+++ index.js
@@ -0,0 +1,7 @@
+const openModal = document.getElementById("open-modal");
+const closeModal = document.getElementById("close-modal");
+const modal = document.getElementById("modal");
+
+openModal.addEventListener("click", () => {
+  
+})
